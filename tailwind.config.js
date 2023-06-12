e73df241ab31a449224197514f15d09047b44ff5
MunBrian <bryanmungai254@gmail.com>
2023-06-12 13:44:45 +0300

    flowbite changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-      "./node_modules/flowbite/**/*.js",
-    ],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     fontFamily: {
-      'body': [
-        'Lato', 
-        'Poppins', 
-        'Roboto',
-        'system-ui',
+      body: [
+        "Lato",
+        "Poppins",
+        "Roboto",
+        "system-ui",
         // other fallback fonts
       ],
     },
     colors: {
-      primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a"}
+      primary: {
+        50: "#eff6ff",
+        100: "#dbeafe",
+        200: "#bfdbfe",
+        300: "#93c5fd",
+        400: "#60a5fa",
+        500: "#3b82f6",
+        600: "#2563eb",
+        700: "#1d4ed8",
+        800: "#1e40af",
+        900: "#1e3a8a",
+      },
     },
   },
-  plugins: [
-    require("flowbite/plugin")
-  ],
-}
-
+  plugins: [require("flowbite/plugin")],
+};
