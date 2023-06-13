8d37240fe5dfef720f1661458a5da5b93df6f8ec
MunBrian <bryanmungai254@gmail.com>
2023-06-13 21:19:46 +0300

    flowbite dropdown update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 export default {
   content: [
     "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite/**/*.{js,jsx,ts,tsx}",
     "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
